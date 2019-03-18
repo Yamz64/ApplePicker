a0d4e51ba4573947a08e8fb9358cba814140386e
James Lillicrap <jamesplillicrap@gmail.com>
2019-03-17 21:47:40 -0400

    2B.3.1
    Added Basket to Death Animation

---
+++ Assets/Scripts/BowlBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlBehavior : MonoBehaviour
+{
+    public float velocity;
+    public float horizontalForce;
+    public Vector2 horizontalBounds;
+    public Vector2 verticalBounds;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2((Random.Range(horizontalBounds[0], horizontalBounds[1]) * horizontalForce), Random.Range(verticalBounds[0], verticalBounds[1]) * velocity));
+        rb.AddTorque(Random.Range(-360.0f, 360.0f));
+    }
+}
--- Assets/Scripts/DeadPlayerBehavior.cs
+++ Assets/Scripts/DeadPlayerBehavior.cs
@@ -5,31 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class DeadPlayerBehavior : MonoBehaviour
 {
-    public float velocity;
-    public Sprite[] deathposes;
-    private SpriteRenderer deathpose;
-    private Rigidbody2D rb;
+    public float velocity;              //float value stores the scaler to the upward force
+    public float offset;                //y distance from object center
+    public Sprite[] deathposes;         //array stores all of the sprites for the death animation
+    public Object bowl;                 //bowl prefab
+    private SpriteRenderer deathpose;   //the spriterenderer component attached to the dead player prefab
+    private Rigidbody2D rb;             //the Rigidbody2D component attached to the dead player prefab
 
+    //DeathSequence() is the function responsible for all of the visual parts to the death animation
     IEnumerator DeathSequence()
     {
-        deathpose.sprite = deathposes[0];
-        rb.gravityScale = 0;
-        yield return new WaitForSeconds(1);
-        deathpose.sprite = deathposes[1];
-        rb.gravityScale = 4;
-        rb.AddForce(transform.up * velocity);
-    }
-    IEnumerator Death()
-    {
-        yield return new WaitForSeconds(2.5f);
-        SceneManager.LoadScene("Game");
+        deathpose.sprite = deathposes[0];       //set the deathpose to the surprised charred state
+        rb.gravityScale = 0;                    //set the gravityScale to 0
+        yield return new WaitForSeconds(1);     //wait 1 second
+        deathpose.sprite = deathposes[1];       //set the deathpose to the normal charred state
+        rb.gravityScale = 4;                    //set the gravityScale to 4
+        rb.AddForce(transform.up * velocity);   //force the rigidbody up scaled by the velocity variable
     }
 
+    //Death() is the function responsible for loading the scene again
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(2.5f);  //wait 2.5 seconds
+        SceneManager.LoadScene("Game");         //reload the scene
+    }
+
+    //Start() occurs on the first frame
     private void Start()
     {
-        deathpose = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(DeathSequence());
-        StartCoroutine(Death());
+        Instantiate(bowl, new Vector3(transform.position.x, transform.position.y + offset, transform.position.z), transform.rotation);   //instantiates bowl prefab at every position except y which is added by offset
+        deathpose = GetComponent<SpriteRenderer>(); //deathpose is set to the SpriteRenderer component
+        rb = GetComponent<Rigidbody2D>();           //rb is set to the Rigidbody2D component attached to 
+        StartCoroutine(DeathSequence());            //initialize DeathSequence()
+        StartCoroutine(Death());                    //initialize Death()
     }
 }
